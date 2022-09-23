a378f7d27c4c689507313aa1e3cb7123b3ed4208
DohtJeb <david.jeggings@gmail.com>
2022-09-23 12:43:55 +0200

    Added scene changing mechanic

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    private int nextIndex;
+    private int index;
     // Start is called before the first frame update
     void Start()
     {
-        
+        index = SceneManager.GetActiveScene().buildIndex;
+        nextIndex = index + 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Door"))
+        {
+            Debug.Log("Entered Door");
+            SceneManager.LoadScene(nextIndex);
+        }
     }
 }
