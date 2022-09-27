3e32c5849b2ee8baa3d15889a1bb986d918163ae
DohtJeb <david.jeggings@gmail.com>
2022-09-27 11:45:31 +0200

    Added prefabs for bottom, middle, and top parts of the ladder

--- Assets/Scripts/DoorAndKey.cs
+++ Assets/Scripts/DoorAndKey.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChange : MonoBehaviour
+public class DoorAndKey : MonoBehaviour
 {
+    
+    private bool hasKey = false;
+
     private int nextIndex;
     private int index;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +27,23 @@ public class SceneChange : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.CompareTag("Door"))
+        if(col.gameObject.CompareTag("Key"))
+        {
+            hasKey = true;
+            Debug.Log("Picked up key");
+            Destroy(col.gameObject);
+        }
+
+        if(col.gameObject.CompareTag("Door") && hasKey == true)
         {
             Debug.Log("Entered Door");
             SceneManager.LoadScene(nextIndex);
         }
+
+        if(col.gameObject.CompareTag("Door") && hasKey == false)
+        {
+            Debug.Log("Door is locked");
+        }
     }
+
 }
