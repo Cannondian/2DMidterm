ff669509337ca7f4ff43d227f7693f8744a6c3c1
Janeil <jkspei21@stlawu.edu>
2022-10-07 01:51:34 +0200

    Victory scene

---
+++ Assets/Scripts/ShowText.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowText : MonoBehaviour
+{
+
+    public string textValue;
+    public TMP_Text textElement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textElement.text = textValue;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
